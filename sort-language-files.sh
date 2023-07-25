e708c8dbbc5dedd0f8e5ad06f2b2ef044b354490
Khaled-MS <96909888+Khaled-MS@users.noreply.github.com>
2023-07-25 23:51:41 +0300

    Add script

---
+++ sort-language-files.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+filesPath=<path-to-files-directory-here>
+
+## Get a list of languages from the files:
+# -> List all files in the directory
+#     -> Get the file names
+#         -> Cut out the numbering and only get the language names
+#             -> Cut out the duplicate values and get unique language names
+#                 -> Cut out the header line to end up with a list of languages
+# Then, input the language names into a variable, languageList
+
+languageList=$(ls -l $filesPath | awk '{print $NF}' | awk -F "-[1-9]" '{print $1}' | uniq | sed '1d')
+
+
+## Loop through languages one by one, sorting each file in its language directory,
+#  -> We create the directory if it does not exist
+#     -> We move each file from the top level directory to each language's directory
+
+cd $filesPath
+
+for language in $languageList
+do
+  echo "Sorting $language language files..."
+
+  if [ ! -d "$language" ]; then
+    mkdir ./$language
+fi
+
+  mv ./"${language}"-* ./$language/
+
+done
+
+echo "Done."
